716997cf9c64856fe5e2d67804541f8e637d15db
ColdIcyDays <erik.ljungman@gmail.com>
2026-06-12 16:04:31 +0200

    Pushed so it works with oracle cloud (don't forget to change current target in vite.config.ts!)
     - Title.

--- src/TestComps/TestLogin.tsx
+++ src/TestComps/TestLogin.tsx
@@ -1,55 +1,174 @@
 ﻿import {useEffect, useState} from "react";
 
+type AccountInfo = {
+    username: string,
+    email: string,
+    roles: string[]
+
+}
+
+type ModifyUserRole = {
+    targetUsername: string,
+    newRoleName: string
+}
+
 export function TestLogin(){
     const [isLoginSuccess, setIsLoginSuccess] = useState<boolean>(false);
+    const [hasCheckedAuth, setHasCheckedAuth] = useState<boolean>(false);
+    const [canModify, setCanModifiy] = useState<boolean>(false);
+    const [currentAccount, setCurrentAccount] = useState<AccountInfo>();
     function HandleLogin(event: React.SyntheticEvent<HTMLFormElement>){
         console.log("Handling login!")
         event.preventDefault();
         const form = event.currentTarget;
         const foundData = new FormData(form);
         foundData.set("rememberMe", (foundData.get("rememeberMe") === "on" ? "true" : "false"));
-        fetch("http://localhost:5294/Account/Auth/Login", {method: 'post', body: foundData})
+        fetch('/Account/Auth/Login', {method: 'post', body: foundData, credentials: 'include'})
+            .then(r => {
+                setIsLoginSuccess(r.ok);
+
+                fetch('/Account/Auth/AccountInfo', {method: 'get', credentials: 'include'})
+                    .then(async r => {
+
+                        const indata = ((await r.json()) as AccountInfo);
+
+                        console.log(indata);
+
+                        setCurrentAccount(indata);
+                    });
+
+                const temp: ModifyUserRole = {} as ModifyUserRole;
+                temp.newRoleName = "Prutt";
+                temp.targetUsername = "Test";
+                fetch('/Account/Auth/ModifyUserRole', {method: 'post', credentials: 'include',
+                    headers: {
+                        "Content-Type": "application/json",
+                    }, body: JSON.stringify(temp) })
+                    .then(async r => {
+                        setCanModifiy(r.ok);
+                    });
+            });
+    }
+
+    function HandleRegister(event: React.SyntheticEvent<HTMLFormElement>){
+        console.log("Handling login!")
+        event.preventDefault();
+        const form = event.currentTarget;
+        const foundData = new FormData(form);
+        fetch('/Account/Auth/Register', {method: 'post', body: foundData, credentials: 'include'})
             .then(r => {
                 setIsLoginSuccess(r.ok);
             });
     }
 
+
+
     useEffect(() => {
-        fetch("http://localhost:5294/Account/Auth/CheckAuth", {method: 'get', credentials: 'include'})
-            .then(r => {
-                setIsLoginSuccess(r.ok);
-            })
+/*        let headers = new Headers();
+        headers.append("Access-Control-Allow-Origin", "http://localhost:5294/");
+        headers.append("Access-Control-Allow-Credentials", "true");*/
+
+        if (!hasCheckedAuth)
+        {
+            fetch('/Account/Auth/CheckAuth', {method: 'get', credentials: 'include'})
+                .then(r => {
+                    console.log("Response recieved!")
+                    console.log(r);
+                    setIsLoginSuccess(r.ok);
+
+                    setHasCheckedAuth(true);
+                    if (r.ok)
+                    {
+                        const temp: ModifyUserRole = {} as ModifyUserRole;
+                        temp.newRoleName = "Prutt";
+                        temp.targetUsername = "Test";
+                        fetch('/Account/Auth/ModifyUserRole', {method: 'post', credentials: 'include',
+                            headers: {
+                                "Content-Type": "application/json",
+                            }, body: JSON.stringify(temp) })
+                            .then(async r => {
+                                setCanModifiy(r.ok);
+                            });
+                        fetch('/Account/Auth/AccountInfo', {method: 'get', credentials: 'include'})
+                            .then(async r => {
+
+                                const indata = ((await r.json()) as AccountInfo);
+
+                                console.log(indata);
+
+                                setCurrentAccount(indata);
+                            });
+
+
+                    }
+                })
+        }
     })
 
     return (
       <>
         <div className="flex flex-col">
-            {isLoginSuccess ?
+            {canModify ?
                 <div>
-                    <h1>You are logged in as: {}</h1>
+                    <h1>You are admin!</h1>
+                </div>
+                :
+                <div>
+                    <h1>You are NOT admin!</h1>
+                </div>}
+            {isLoginSuccess && currentAccount ?
+                <div>
+                    <h1>You are logged in as: {currentAccount.username}</h1>
+                    <h1>Email: {currentAccount.email}</h1>
+                    <h1>Roles {currentAccount.roles.map((v, idx) => {
+                        return (v)
+                    })}</h1>
                 </div>
                 : null
             }
-            <form className={"ml-4"} id={"login"} onSubmit={HandleLogin} action={""}>
-                <div>
-                    <label className={"block"}>Username</label>
-                    <input className={"border"} type={"text"} form={"login"} name={"userName"}/>
-                </div>
+
+            <div className={'flex flex-row'}>
+                <form className={"ml-4"} id={"login"} onSubmit={HandleLogin} action={""}>
+                    <h1>Login</h1>
+                    <div>
+                        <label className={"block"}>Username</label>
+                        <input className={"border"} type={"text"} form={"login"} name={"userName"}/>
+                    </div>
+
+                    <div>
+                        <label className={"block"}>Password</label>
+                        <input className={"border"} type={"text"} form={"login"} name={"password"}/>
+                    </div>
+
+                    <div>
+                        <label>Remember me</label>
+                        <input className={"ml-4"} type={"checkbox"} form={"login"} name={"rememberMe"}/>
+                    </div>
+
+                    <input type={"submit"} form={"login"} className={"cursor-pointer"}/>
+                </form>
 
                 <div>
-                    <label className={"block"}>Password</label>
-                    <input className={"border"} type={"text"} form={"login"} name={"password"}/>
+                    <form className={"ml-4"} id={"register"} onSubmit={HandleRegister} action={""}>
+                    <h1>Register</h1>
+                        <div>
+                            <label className={"block"}>Email</label>
+                            <input className={"border"} type={"text"} form={"register"} name={"email"}/>
+                        </div>
+
+                        <div>
+                            <label className={"block"}>Username</label>
+                            <input className={"border"} type={"text"} form={"register"} name={"userName"}/>
+                        </div>
+
+                        <div>
+                            <label className={"block"}>Password</label>
+                            <input className={"border"} type={"text"} form={"register"} name={"password"}/>
+                        </div>
+
+                        <input type={"submit"} form={"register"} className={"cursor-pointer"}/>
+                    </form>
                 </div>
-
-                <div>
-                    <label>Remember me</label>
-                    <input className={"ml-4"} type={""} form={"login"} name={"rememberMe"}/>
-                </div>
-
-                <input type={"submit"} form={"login"} className={"cursor-pointer"}/>
-            </form>
-            <div>
-
             </div>
         </div>
       </>
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,87 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
+import fs from "fs"
+
+import path from "path"
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_")
+  const currentTarget : string = "http://localhost:5003"
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      babel({ presets: [reactCompilerPreset()]})
+    ],
+    server: {
+      host: "0.0.0.0",
+      proxy: {
+        '/WeatherForecast': {
+          target: currentTarget,
+          changeOrigin: true,
+          secure: false
+        },
+        '/Account/Auth/Login': {
+          target: currentTarget,
+          changeOrigin: true,
+          secure: false,
+          configure: (proxy, _options) => {
+            proxy.on('proxyReq', (proxyReq, req, res) => {
+              proxyReq.setHeader("origin", currentTarget);
+            })
+          }
+        },
+        '/Account/Auth/AccountInfo': {
+          target: currentTarget,
+          changeOrigin: true,
+          secure: false,
+          configure: (proxy, _options) => {
+            proxy.on('proxyReq', (proxyReq, req, res) => {
+              proxyReq.setHeader("origin", currentTarget);
+            })
+          }
+        },
+        '/Account/Auth/CheckAuth': {
+          target: currentTarget,
+          changeOrigin: true,
+          secure: false,
+          configure: (proxy, _options) => {
+            proxy.on('proxyReq', (proxyReq, req, res) => {
+              proxyReq.setHeader("origin", currentTarget);
+            })
+          }
+        },
+        '/Account/Auth/ModifyUserRole': {
+          target: currentTarget,
+          changeOrigin: true,
+          secure: false,
+          configure: (proxy, _options) => {
+            proxy.on('proxyReq', (proxyReq, req, res) => {
+              proxyReq.setHeader("origin", currentTarget);
+            })
+          }
+        },
+        '/Account/Auth/Register': {
+          target: currentTarget,
+          changeOrigin: true,
+          secure: false,
+          configure: (proxy, _options) => {
+            proxy.on('proxyReq', (proxyReq, req, res) => {
+              proxyReq.setHeader("origin", currentTarget);
+            })
+          }
+        }
+      }
+    }
+  }
+})
+
+
+/*
+* import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from "@tailwindcss/vite";
@@ -11,17 +94,64 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()]})
   ],
   server: {
+    host: "0.0.0.0",
     proxy: {
       '/WeatherForecast': {
-        target: 'http://localhost:5294',
+        target: 'http://lexicon_orchbookingbackend:8080',
         changeOrigin: true,
         secure: false
       },
       '/Account/Auth/Login': {
-        target: 'http://localhost:5294',
+        target: 'http://lexicon_orchbookingbackend:8080',
         changeOrigin: true,
-        secure: false
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader("origin", "http://lexicon_orchbookingbackend:8080");
+          })
+        }
+      },
+      '/Account/Auth/AccountInfo': {
+        target: 'http://lexicon_orchbookingbackend:8080',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader("origin", "http://lexicon_orchbookingbackend:8080");
+          })
+        }
+      },
+      '/Account/Auth/CheckAuth': {
+        target: 'http://lexicon_orchbookingbackend:8080',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader("origin", "http://lexicon_orchbookingbackend:8080");
+          })
+        }
+      },
+      '/Account/Auth/ModifyUserRole': {
+        target: 'http://lexicon_orchbookingbackend:8080',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader("origin", "http://lexicon_orchbookingbackend:8080");
+          })
+        }
+      },
+      '/Account/Auth/Register': {
+        target: 'http://lexicon_orchbookingbackend:8080',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader("origin", "http://lexicon_orchbookingbackend:8080");
+          })
+        }
       }
     }
   }
 })
+*/
