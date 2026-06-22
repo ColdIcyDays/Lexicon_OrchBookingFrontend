03ee5c31bacd44e0853088c54cbe1a2411f207d1
Erik Lj <erik.ljungman@gmail.com>
2026-06-22 19:15:17 +0200

    AuthCompGuard saves to local storage if not saved
     - Title.

--- src/components/guard/AuthCompGuard.tsx
+++ src/components/guard/AuthCompGuard.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {OrchBookHelper} from "../../helpers/FetchHelpers.tsx";
 import {Navigate, useLocation} from "react-router-dom";
+import {OrchLocalStorageHelpers} from "../../helpers/OrchLocalStorageHelpers.tsx";
 
 export function AuthCompGuard({ LoggedInComp, LoggedOutComp } : { LoggedInComp : React.ReactNode, LoggedOutComp : React.ReactNode }){
     const [isAuthed, setIsAuthed] = useState<boolean>(false);
@@ -11,12 +12,30 @@ export function AuthCompGuard({ LoggedInComp, LoggedOutComp } : { LoggedInComp :
     useEffect(() =>
     {
         let ignore = false;
-        OrchBookHelper.OrchFetchGet('/Account/Auth/CheckAuth').then(r =>
+        OrchBookHelper.OrchFetchCheckAuth().then(r =>
         {
             if (!ignore)
             {
                 setIsWaiting(false);
                 setIsAuthed(r.ok);
+
+                if (OrchLocalStorageHelpers.GetAccountInfo() === null)
+                {
+                    OrchBookHelper.OrchFetchGet("/Account/Auth/AccountInfo")
+                        .then(accountR =>
+                        {
+                            if (!ignore)
+                            {
+                                accountR.json().then(j =>
+                                {
+                                    if (!ignore)
+                                    {
+                                        OrchLocalStorageHelpers.SaveAccountInfoLocally(j)
+                                    }
+                                })
+                            }
+                        })
+                }
             }
         });
 
