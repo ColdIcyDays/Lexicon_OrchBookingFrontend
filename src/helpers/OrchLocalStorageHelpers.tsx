ea33e42cef653b378e8c85565d2058f3a813a2d2
Erik Lj <erik.ljungman@gmail.com>
2026-06-22 19:09:17 +0200

    Added local storage helpers
     - Title.
     - Mainly to handle local storage

---
+++ src/helpers/OrchLocalStorageHelpers.tsx
@@ -0,0 +1,38 @@
+﻿import type {GetAccountInfoResult} from "../Types/UserTypes.tsx";
+import {OrchBookHelper} from "./FetchHelpers.tsx";
+
+export abstract class OrchLocalStorageHelpers
+{
+    private static AccountInfoKey : string = "AccountInfo"
+    /* TODO: This needs to be called somewhere, and update when the user changes... Probably move CheckAuth to its own helper function? */
+    public static SaveAccountInfoLocally(someInfo : GetAccountInfoResult){
+        localStorage.setItem(OrchLocalStorageHelpers.AccountInfoKey, JSON.stringify(someInfo));
+    }
+
+    public static ClearAccountInfo(){
+        localStorage.removeItem(OrchLocalStorageHelpers.AccountInfoKey)
+    }
+
+    public static FetchAndSaveAccountInfoLocally(){
+        OrchBookHelper.OrchFetchGet("/Account/Auth/GetAccountInfo")
+            .then(r =>
+            {
+                r.json()
+                    .then(j =>
+                    {
+                        this.SaveAccountInfoLocally(j as GetAccountInfoResult);
+                    })
+            })
+    }
+
+    public static GetAccountInfo() : GetAccountInfoResult | null
+    {
+        const data = localStorage.getItem(OrchLocalStorageHelpers.AccountInfoKey);
+        if (data === null)
+        {
+            return null;
+        }
+
+        return JSON.parse(data);
+    }
+}
