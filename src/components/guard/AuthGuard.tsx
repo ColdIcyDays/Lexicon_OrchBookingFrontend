46ca3b7f2def57dbc8d368246f2571adf938a234
Erik Lj <erik.ljungman@gmail.com>
2026-06-23 15:23:12 +0200

    Added authguard comp for roles
     - Title.

---
+++ src/components/guard/AuthGuard.tsx
@@ -0,0 +1,13 @@
+﻿import {type ReactNode, useEffect} from "react";
+
+export function AuthGuard({ children, RequiredRole } : { children : ReactNode | ReactNode[] , RequiredRole : string }) {
+
+    useEffect(() =>
+    {
+        
+    }, []);
+
+    return (
+        {children}
+    );
+}
